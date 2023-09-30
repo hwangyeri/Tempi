f289a788ef69b4c240543612a9c31ded8608be49
hwangyeri <dpfl420@naver.com>
2023-09-30 19:22:26 +0900

    [Style] Design Sysytem Constant 생성
    - TButton
    - SFSymbol

---
+++ Tempi/Tempi/Source/Utility/DesignSystem/Constant/Constant.swift
@@ -0,0 +1,51 @@
+//
+//  Constant.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/09/30.
+//
+
+import Foundation
+
+enum Constant {
+    
+    enum TButton {
+        static let cornerRadius: CGFloat = 100
+    }
+    
+    enum SFSymbol {
+        /// Display Icon
+        static let searchBarIcon: String = "magnifyingglass"
+        static let plusCircleIcon: String = "plus.circle.fill"
+        static let plusSquareIcon: String = "plus.square.fill" // plus.app.fill
+        static let checkBoxIcon: String = "checkmark.square.fill"
+        static let emptyBoxIcon: String = "square"
+        static let editIcon: String = "pencil.line"
+        static let deleteIcon: String = "trash"
+        static let fixedIcon: String = "pin.fill"
+        static let unfixedIcon: String = "pin"
+        static let categoryEditIcon: String = "list.bullet"
+        static let bookmarkIcon: String = "note.text.badge.plus"
+        static let checkboxMenuIcon: String = "ellipsis"
+        static let alarmIcon: String = "alarm"
+        static let alarmMessageIcon: String = "message"
+        static let alarmCheckIcon: String = "message"
+        static let infoIcon: String = "checkmark"
+        
+        /// Category Icon
+        static let travelCategoryIcon: String = "airplane.departure"
+        static let shoppingCategoryIcon: String = "cart.fill"
+        static let wishListCategoryIcon: String = "sparkles"
+        static let studyCategoryIcon: String = "text.book.closed.fill"
+        static let removalCategoryIcon: String = "box.truck.fill"
+        static let exerciseCategoryIcon: String = "dumbbell.fill"
+        
+        /// CheckBox Drop-down Menu Icon
+        static let editCheckBoxIcon: String = "pencil"
+        static let memoCheckBoxIcon: String = "note.text"
+        static let alarmCheckBoxIcon: String = "alarm"
+        static let bookmarkCheckBoxIcon: String = "star"
+        static let deletemarkCheckBoxIcon: String = "trash"
+    }
+    
+}
