c6b337d1fa769d19fcd147ed06f223209c47be8e
hwangyeri <dpfl420@naver.com>
2023-10-01 18:47:22 +0900

    [Feat] Component TLabel 생성

---
+++ Tempi/Tempi/Source/Utility/DesignSystem/Component/TLabel.swift
@@ -0,0 +1,25 @@
+//
+//  UILabel.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/10/01.
+//
+
+import UIKit
+
+final class TLabel: UILabel {
+    
+    init(text: String, custFont: UIFont.CustomFont, textColor: UIColor) {
+        super.init(frame: .zero)
+        self.text = text
+        self.font = .customFont(custFont)
+        self.textColor = textColor
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
