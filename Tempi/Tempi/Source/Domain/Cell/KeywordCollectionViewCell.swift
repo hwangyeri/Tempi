a3e19a7f0a73a0b8d342f22ddcb82d136114dfd7
hwangyeri <dpfl420@naver.com>
2023-10-03 17:06:41 +0900

    [Style] KeywordCollectionViewCell UI 구현 (진행중)
    - Border 이슈

--- Tempi/Tempi/Source/Domain/Cell/KeywordCollectionViewCell.swift
+++ Tempi/Tempi/Source/Domain/Cell/KeywordCollectionViewCell.swift
@@ -10,28 +10,44 @@ import SnapKit
 
 class KeywordCollectionViewCell: BaseCollectionViewCell {
     
-    // FIXME: Border 가 안보임...
+    // FIXME: Border 가 안보임... Color 변경해야 함
+    
+    let borderView = {
+        let view = UIView()
+        view.layer.cornerRadius = Constant.TKeywordLabel.cornerRadius
+        view.layer.borderWidth = Constant.TKeywordLabel.borderWidth
+        view.layer.borderColor = UIColor.red.cgColor
+        view.backgroundColor = .yellow
+        return view
+    }()
 
     let keywordLabel = {
         let view = TLabel(
             text: "테스트",
             custFont: .pretendardSemiBoldXS,
             textColor: .tGray1000)
-        view.layer.cornerRadius = Constant.TKeywordLabel.cornerRadius
-        view.layer.borderColor = UIColor.red.cgColor
-        view.layer.borderWidth = Constant.TKeywordLabel.borderWidth
-        view.backgroundColor = UIColor.tGray100
+//        view.layer.cornerRadius = Constant.TKeywordLabel.cornerRadius
+//        view.layer.borderColor = UIColor.yellow.cgColor
+//        view.layer.borderWidth = Constant.TKeywordLabel.borderWidth
+//        view.backgroundColor = UIColor.lightGray
+        view.numberOfLines = 0
+        view.backgroundColor = .clear
         return view
     }()
     
     override func configureHierarchy() {
-        contentView.addSubview(keywordLabel)
+        contentView.addSubview(borderView)
+        borderView.addSubview(keywordLabel)
     }
     
     override func configureLayout() {
-        keywordLabel.snp.makeConstraints { make in
+        borderView.snp.makeConstraints { make in
             make.edges.equalTo(contentView)
         }
+        
+//        keywordLabel.snp.makeConstraints { make in
+//            make.edges.equalTo(borderView)
+//        }
     }
     
 }
