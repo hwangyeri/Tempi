e90ce327f65f14894e6e2b517dcdae389fe87709
hwangyeri <dpfl420@naver.com>
2023-10-03 17:05:29 +0900

    [Feat] CategoryType Model 생성

---
+++ Tempi/Tempi/Source/Domain/Model/Category.swift
@@ -0,0 +1,50 @@
+//
+//  Category.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/10/02.
+//
+
+import UIKit
+
+enum CategoryType: String {
+    case travel
+    case shopping
+    case wishList
+    case bucketList
+    case cleaning
+    case move
+    case beforeGoingOut
+    case health
+    
+    static let categories: [CategoryType] = [.travel, .shopping, .wishList, .bucketList, .cleaning, .move, .beforeGoingOut, .health]
+    
+    var image: UIImage? {
+        switch self {
+        case .travel:
+            return UIImage(systemName: Constant.SFSymbol.travelCategoryIcon)
+        case .shopping:
+            return UIImage(systemName: Constant.SFSymbol.shoppingCategoryIcon)
+        case .wishList:
+            return UIImage(systemName: Constant.SFSymbol.wishListCategoryIcon)
+        case .bucketList:
+            return UIImage(systemName: Constant.SFSymbol.bucketListCategoryIcon)
+        case .cleaning:
+            return UIImage(systemName: Constant.SFSymbol.cleaningCategoryIcon)
+        case .move:
+            return UIImage(systemName: Constant.SFSymbol.moveCategoryIcon)
+        case .beforeGoingOut:
+            return UIImage(systemName: Constant.SFSymbol.beforeGoingOutCategoryIcon)
+        case .health:
+            return UIImage(systemName: Constant.SFSymbol.healthCategoryIcon)
+        }
+    }
+    
+    var text: String {
+        switch self {
+        case .travel, .shopping, .wishList, .bucketList, .cleaning, .beforeGoingOut, .health, .move:
+            return "category_\(self.rawValue)".localized
+        }
+    }
+    
+}
