34c05d6ca43711aa7bbb0a0e148c5f125b786914
hwangyeri <dpfl420@naver.com>
2023-10-04 18:16:24 +0900

    [Style] Component TButton 생성

---
+++ Tempi/Tempi/Source/Utility/DesignSystem/Component/TButton.swift
@@ -0,0 +1,28 @@
+//
+//  TButton.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/10/04.
+//
+
+import UIKit
+
+final class TButton: UIButton {
+    
+    init(text: String) {
+        super.init(frame: .zero)
+        self.setTitle(text, for: .normal)
+        self.setTitleColor(UIColor.tGray100, for: .normal)
+        self.backgroundColor = UIColor.tGray1000
+        self.layer.cornerRadius = Constant.TButton.cornerRadius
+        self.titleLabel?.font = .customFont(.pretendardSemiBoldXL)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
+
+
