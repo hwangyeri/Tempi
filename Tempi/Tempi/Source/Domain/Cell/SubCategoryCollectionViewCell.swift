7247b08090668b21e2744b03784316ad6ad67f4e
hwangyeri <dpfl420@naver.com>
2023-10-04 19:54:42 +0900

    [Style] SubCategoryCollectionViewCell UI 구현

---
+++ Tempi/Tempi/Source/Domain/Cell/SubCategoryCollectionViewCell.swift
@@ -0,0 +1,45 @@
+//
+//  SubCategoryCollectionViewCell.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/10/04.
+//
+
+import UIKit
+import SnapKit
+
+class SubCategoryCollectionViewCell: BaseCollectionViewCell {
+    
+    let backView = {
+        let view = UIView()
+        view.layer.cornerRadius = Constant.TSubCategory.cornerRadius
+        view.backgroundColor = UIColor.tGray200
+        return view
+    }()
+    
+    let textLabel = {
+        let view = TLabel(
+            text: "test",
+            custFont: .pretendardRegularL,
+            textColor: .tGray600)
+        view.backgroundColor = UIColor.clear
+        view.textAlignment = .center
+        return view
+    }()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(backView)
+        backView.addSubview(textLabel)
+    }
+    
+    override func configureLayout() {
+        backView.snp.makeConstraints { make in
+            make.edges.equalTo(contentView)
+        }
+        
+        textLabel.snp.makeConstraints { make in
+            make.edges.equalTo(backView).inset(10)
+        }
+    }
+    
+}
