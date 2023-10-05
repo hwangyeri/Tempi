013fb9cc2964906ca273b6290541ed848c56a169
hwangyeri <dpfl420@naver.com>
2023-10-05 22:43:37 +0900

    [Style] TLabel Component - numberOfLines 추가

--- Tempi/Tempi/Source/Utility/DesignSystem/Component/TLabel.swift
+++ Tempi/Tempi/Source/Utility/DesignSystem/Component/TLabel.swift
@@ -11,9 +11,11 @@ final class TLabel: UILabel {
     
     init(text: String, custFont: UIFont.CustomFont, textColor: UIColor) {
         super.init(frame: .zero)
+       
         self.text = text
         self.font = .customFont(custFont)
         self.textColor = textColor
+        self.numberOfLines = 0
     }
     
     required init?(coder aDecoder: NSCoder) {
