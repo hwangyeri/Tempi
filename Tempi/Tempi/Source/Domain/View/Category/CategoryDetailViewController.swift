ad953cd92e80c44be57069c32761f61808f680b0
hwangyeri <dpfl420@naver.com>
2023-10-11 00:16:15 +0900

    [Feat] CategoryDetail - subCategory 선택 로직 및 UI 업데이트 기능 구현
    - 서브 카테고리 선택 시 UI 업데이트 및 선택 로직 추가
    - 서브 카테고리 선택 시 UI 스타일 변경 및 선택 상태 관리 추가
    - 다음 화면으로 이동하는 버튼 로직 구현
    - 다음 버튼 비활성화 및 UI 업데이트 적용
    - CollectionView didSelectItemAt 값 전달 및 화면 전환 기능 구현
    - dummyData -> CategoryDefaultData 데이터 변경
    - dummyData 관련 코드 주석처리, 불필요한 코드 정리
    - 카테고리 홈 화면, 접근 제어자 추가

--- Tempi/Tempi/Source/Domain/View/Category/CategoryDetailViewController.swift
+++ Tempi/Tempi/Source/Domain/View/Category/CategoryDetailViewController.swift
@@ -9,20 +9,37 @@ import UIKit
 
 class CategoryDetailViewController: BaseViewController {
     
-    let dummyList = ["일본 여행", "국내 여행", "djdjdjdjdjdjdjdnjdndjfd dnjdjd수영장", "어쩌구 저쩌구 호캉스", "어쩌구 휴양지", "2자"]
+    var categoryName: String?
     
-    let mainView = CategoryDetailView()
+    private var subCategoryList: [String] {
+        // categoryName에 해당하는 subCategoryName만 필터링
+        return DataManager.shared.categoryList
+            .filter { $0.categoryName == categoryName }
+            .map { $0.subCategoryName }
+    }
+    
+    private var selectedIndexPath: IndexPath?
+    
+    private let mainView = CategoryDetailView()
     
     private var subCategoryDataSource: UICollectionViewDiffableDataSource<Int, String>!
     
     override func loadView() {
         self.view = mainView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureSubCategoryDataSource()
+        
+        //        print("---- DetailView", categoryName)
+        //        print(subCategoryList)
+    }
+    
+    override func configureHierarchy() {
+        mainView.subCategoryCollectionView.delegate = self
+        mainView.tButton.addTarget(self, action: #selector(nextButtonTapped), for: .touchUpInside)
     }
     
     private func configureSubCategoryDataSource() {
@@ -38,8 +55,56 @@ class CategoryDetailViewController: BaseViewController {
         
         var snapshot = NSDiffableDataSourceSnapshot<Int, String>()
         snapshot.appendSections([0])
-        snapshot.appendItems(dummyList)
+        let uniqueItems = Set(subCategoryList) // 중복 제거
+        snapshot.appendItems(Array(uniqueItems))
         subCategoryDataSource.apply(snapshot)
+    }
+    
+    @objc private func nextButtonTapped() {
+        if let selectedIndexPath = selectedIndexPath, let selectedSubCategory = subCategoryDataSource.itemIdentifier(for: selectedIndexPath) {
+            let categoryChecklistVC = CategoryChecklistViewController()
+            categoryChecklistVC.categoryName = categoryName
+            categoryChecklistVC.subCategoryName = selectedSubCategory
+            navigationController?.pushViewController(categoryChecklistVC, animated: true)
+        }
+    }
+    
+}
+
+// MARK: - CollectionView Delegate
+
+extension CategoryDetailViewController: UICollectionViewDelegate {
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print(#function)
+        
+        if let cell = collectionView.cellForItem(at: indexPath) as? SubCategoryCollectionViewCell {
+            // 이전에 선택된 cell UI 원래대로 복원
+            if let prevSelectedIndexPath = selectedIndexPath,
+               let prevSelectedCell = collectionView.cellForItem(at: prevSelectedIndexPath) as? SubCategoryCollectionViewCell {
+                prevSelectedCell.backView.backgroundColor = .tGray200
+                prevSelectedCell.textLabel.textColor = .tGray600
+                prevSelectedCell.textLabel.font = .customFont(.pretendardRegularL)
+            }
+            
+            // 현재 선택된 cell UI 변경
+            cell.backView.backgroundColor = .tGray1000
+            cell.textLabel.textColor = .tGray100
+            cell.textLabel.font = .customFont(.pretendardSemiBoldL)
+            
+            // 현재 선택된 indexPath 저장
+            selectedIndexPath = indexPath
+            print(selectedIndexPath)
+            
+            // tButton Disable 처리
+            if selectedIndexPath == nil {
+                mainView.tButton.isEnabled = false
+                mainView.tButton.backgroundColor = .tGray200
+            } else {
+                mainView.tButton.isEnabled = true
+                mainView.tButton.backgroundColor = .tGray1000
+            }
+        }
     }
     
 }
