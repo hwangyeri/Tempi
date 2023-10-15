5da593422e1e67c45c6a52036583fc4eb68e9769
hwangyeri <dpfl420@naver.com>
2023-10-16 00:27:36 +0900

    [Refactor] CheckItemTable checklistPK 타입 변경
    - String -> ObjectId

--- Tempi/Tempi/Source/Data/RealmTable/CheckItemTable.swift
+++ Tempi/Tempi/Source/Data/RealmTable/CheckItemTable.swift
@@ -11,14 +11,14 @@ import RealmSwift
 class CheckItemTable: Object {
     
     @Persisted(primaryKey: true) var id: ObjectId
-    @Persisted var checklistPK: String // 속한 체크 리스트 PK
+    @Persisted var checklistPK: ObjectId // 속한 체크 리스트 PK
     @Persisted var content: String // 내용
     @Persisted var createdAt: Date // 생성 날짜
     @Persisted var memo: String? // 메모
     @Persisted var alarmDate: Date? // 알람 설정 날짜, 시간
     @Persisted var isChecked: Bool // 체크 여부
     
-    convenience init(checklistPK: String, content: String, createdAt: Date, memo: String?, alarmDate: Date?, isChecked: Bool) {
+    convenience init(checklistPK: ObjectId, content: String, createdAt: Date, memo: String?, alarmDate: Date?, isChecked: Bool) {
         self.init()
         
         self.checklistPK = checklistPK
