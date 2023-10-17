e712f72feafa3a9d049642eb93c99ce43a61dcf0
hwangyeri <dpfl420@naver.com>
2023-10-17 14:53:25 +0900

    [Feat] CheckItemTableRepository Realm Delete 함수 추가 및 주석 변경
    - checklistPK 를 기준으로 필터링, 해당하는 Check Item 을 삭제하는 함수

--- Tempi/Tempi/Source/Data/Repository/CheckItemTableRepository.swift
+++ Tempi/Tempi/Source/Data/Repository/CheckItemTableRepository.swift
@@ -9,16 +9,16 @@ import Foundation
 import RealmSwift
 
 protocol CheckItemTableRepositoryType: AnyObject {
-    
+    func createItem(_ item: CheckItemTable)
+    func fetch(for checklistID: ObjectId, completion: @escaping (Results<CheckItemTable>?) -> Void)
 }
 
 class CheckItemTableRepository: CheckItemTableRepositoryType {
     
     private let realm = try! Realm()
     
-    // Realm Create
+    // MARK: - Realm Create
     func createItem(_ item: CheckItemTable) {
-        
         do {
             try realm.write {
                 realm.add(item)
@@ -27,17 +27,17 @@ class CheckItemTableRepository: CheckItemTableRepositoryType {
         } catch {
             print(error)
         }
-        
     }
     
-    // Realm Read
+    // MARK: - Realm Read
     func fetch(for checklistID: ObjectId, completion: @escaping (Results<CheckItemTable>?) -> Void) {
-        // checklistID를 기준으로 필터링, 생성순으로 오름차순 정렬 (과거 날짜 -> 최근 날짜)
+        /// checklistID 를 기준으로 필터링
+        /// 생성순으로 오름차순 정렬 (과거 날짜 -> 최근 날짜)
         let filteredData = realm.objects(CheckItemTable.self).filter("checklistPK == %@", checklistID).sorted(byKeyPath: "createdAt", ascending: true)
         completion(filteredData)
     }
     
-    // Realm Update
+    // MARK: - Realm Update
 //    func updateItem(id: ObjectId, title: String, contents: String) {
 //
 //        do {
@@ -52,18 +52,18 @@ class CheckItemTableRepository: CheckItemTableRepositoryType {
 //
 //    }
     
-    // Realm Delete
-//    func deleteItem(_ item: ChecklistTable) {
-//
-//        do {
-//            try realm.write {
-//                realm.delete(item)
-//                print("Realm Delete Succeed")
-//            }
-//        } catch {
-//            print(error)
-//        }
-//
-//    }
+    // MARK: - Realm Delete
+    func deleteItem(forChecklistPK checklistPK: ObjectId) {
+        /// checklistPK 를 기준으로 필터링, 해당하는 Check Item 을 삭제하는 함수
+        let itemsToDelete = realm.objects(CheckItemTable.self).filter("checklistPK == %@", checklistPK)
+        do {
+            try realm.write {
+                realm.delete(itemsToDelete)
+                print("Realm Delete Succeed")
+            }
+        } catch {
+            print(error)
+        }
+    }
     
 }
