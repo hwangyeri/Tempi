1cee6d500d5ddaac2d54deb3d20af90ff985173e
hwangyeri <dpfl420@naver.com>
2023-10-17 14:31:59 +0900

    [Feat] NSNotification extension 생성

---
+++ Tempi/Tempi/Source/Utility/DesignSystem/Extension/NSNotification+.swift
@@ -0,0 +1,12 @@
+//
+//  NSNotification+.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/10/16.
+//
+
+import Foundation
+
+extension NSNotification.Name {
+    static let deleteChecklist = NSNotification.Name("DeleteChecklist")
+}
