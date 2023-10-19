1cf2ec8790ce31642926aef234bc6104c4106248
hwangyeri <dpfl420@naver.com>
2023-10-20 01:48:05 +0900

    [Remove] presentDeleteAlert 관련 주석 제거

--- Tempi/Tempi/Source/Utility/DesignSystem/Extension/Alert+.swift
+++ Tempi/Tempi/Source/Utility/DesignSystem/Extension/Alert+.swift
@@ -9,31 +9,7 @@ import UIKit
 
 extension UIViewController {
     
-    /// 카테고리, 즐겨찾기 삭제 시 쓰이는 Alert
-//    func presentDeleteAlert(title: String, message: String, deleteHandler: @escaping () -> Void) {
-//        let alertController = UIAlertController(
-//            title: title,
-//            message: message,
-//            preferredStyle: .alert
-//        )
-//        
-//        let deleteAction = UIAlertAction(title: "삭제", style: .destructive) { _ in
-//            
-//            deleteHandler()
-//            
-//            NotificationCenter.default.post(name: NSNotification.Name("DataDidChange"), object: nil)
-//        }
-//        
-//        let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-//        
-//        alertController.addAction(deleteAction)
-//        alertController.addAction(cancelAction)
-//        
-//        present(alertController, animated: true, completion: nil)
-//    }
-    
     func showToast(message : String) {
-//        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 100, y: self.view.frame.size.height - 450, width: 200, height: 40))
         let toastLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 220, height: 43))
         toastLabel.center = CGPoint(x: self.view.frame.size.width / 2, y: self.view.frame.size.height / 2)
         toastLabel.backgroundColor = UIColor.tGray1000.withAlphaComponent(0.6)
@@ -53,7 +29,3 @@ extension UIViewController {
     }
     
 }
-
-//presentDeleteAlert(title: "ddd", message: "ddd") {
-//    print(#function)
-//}
