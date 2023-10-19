58c67845cdb85aeb710a2397ee7be6e1b078c31c
hwangyeri <dpfl420@naver.com>
2023-10-20 01:21:41 +0900

    [Feat] 체크아이템 메뉴 버튼 - 내용 수정, 메모 설정 액션 기능 구현
    - 분기처리 및 값 전달
    - 내용 수정/메모 설정 Realm 상호작용 및 UI 업데이트
    - 메뉴 버튼 액션 Toast 메세지, NSNotification 처리
    - 체크박스 상태 활성화/비활성화 Realm 상호작용 및 UI 업데이트
    - UI 관련 코드 비동기 처리

--- Tempi/Tempi/Source/Domain/View/CustomChecklist/ChecklistViewController.swift
+++ Tempi/Tempi/Source/Domain/View/CustomChecklist/ChecklistViewController.swift
@@ -34,6 +34,8 @@ class ChecklistViewController: BaseViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(deleteChecklistNotificationObserver(notification:)), name: NSNotification.Name.deleteChecklist, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(updateChecklistNameNotificationObserver(notification:)), name: NSNotification.Name.updateChecklistName, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(deleteCheckItemNotificationObserver(notification:)), name: NSNotification.Name.deleteCheckItem, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateCheckItemContentNotificationObserver(notification:)), name: NSNotification.Name.updateCheckItemContent, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(updateCheckItemMemoNotificationObserver(notification:)), name: NSNotification.Name.updateCheckItemMemo, object: nil)
     }
     
     deinit {
@@ -43,9 +45,10 @@ class ChecklistViewController: BaseViewController {
     override func configureLayout() {
         mainView.checklistCollectionView.delegate = self
         mainView.checklistNameEditButton.addTarget(self, action: #selector(checklistNameEditButtonTapped), for: .touchUpInside)
+        mainView.bookmarkListButton.addTarget(self, action: #selector(bookmarkListButtonTapped), for: .touchUpInside)
         mainView.checklistFixedButton.addTarget(self, action: #selector(checklistFixedButtonTapped), for: .touchUpInside)
         mainView.checklistDeleteButton.addTarget(self, action: #selector(checklistDeleteButtonTapped), for: .touchUpInside)
-        mainView.bookmarkListButton.addTarget(self, action: #selector(bookmarkListButtonTapped), for: .touchUpInside)
+        mainView.plusButton.addTarget(self, action: #selector(plusButtonTapped), for: .touchUpInside)
     }
     
     private func setChecklistData() {
@@ -55,13 +58,20 @@ class ChecklistViewController: BaseViewController {
         dateFormatter.locale = Locale(identifier: "checklist_checklistDateLabel".localized)
         
         guard let selectedChecklistID = selectedChecklistID else {
+            print("No checklist is selected.")
             return
         }
-        let checklistName = checklistRepository.getChecklistName(forId: selectedChecklistID)
+        
+        guard let checklistName = checklistRepository.getChecklistName(forId: selectedChecklistID) else {
+            print("Checklist name is nil")
+            return
+        }
         
         guard let checklistDate = checklistRepository.getChecklistDate(forId: selectedChecklistID) else {
+            print("Checklist date is nil")
             return
         }
+        
         let dateString = dateFormatter.string(from: checklistDate)
         
         mainView.checklistNameLabel.text = checklistName
@@ -90,6 +100,7 @@ class ChecklistViewController: BaseViewController {
         editChecklistNameVC.modalPresentationStyle = .overCurrentContext
         editChecklistNameVC.selectedChecklistID = selectedChecklistID
         editChecklistNameVC.textFieldPlaceholder = mainView.checklistNameLabel.text
+        editChecklistNameVC.nameAction = .updateChecklistName
         self.present(editChecklistNameVC, animated: true)
     }
     
@@ -111,12 +122,12 @@ class ChecklistViewController: BaseViewController {
                 let imageConfig = UIImage.SymbolConfiguration(pointSize: Constant.TImageButton.checklistImageSize, weight: .regular)
                 let image = UIImage(systemName: Constant.SFSymbol.checklistUnFixedIcon, withConfiguration: imageConfig)
                 self.mainView.checklistFixedButton.setImage(image, for: .normal)
-                self.checklistRepository.updateIsFixed(forId: selectedChecklistID, isFixed: false)
+                self.checklistRepository.updateIsFixed(forId: selectedChecklistID, newIsFixed: false)
             } else {
                 let imageConfig = UIImage.SymbolConfiguration(pointSize: Constant.TImageButton.checklistImageSize, weight: .regular)
                 let image = UIImage(systemName: Constant.SFSymbol.checklistFixedIcon, withConfiguration: imageConfig)
                 self.mainView.checklistFixedButton.setImage(image, for: .normal)
-                self.checklistRepository.updateIsFixed(forId: selectedChecklistID, isFixed: true)
+                self.checklistRepository.updateIsFixed(forId: selectedChecklistID, newIsFixed: true)
             }
         }
     }
@@ -130,7 +141,7 @@ class ChecklistViewController: BaseViewController {
             return
         }
         
-        let popupVC = PopUpViewController()
+        let popupVC = DeletePopUpViewController()
         popupVC.modalTransitionStyle = .crossDissolve
         popupVC.modalPresentationStyle = .overCurrentContext
         popupVC.selectedChecklistID = selectedChecklistID
@@ -152,6 +163,17 @@ class ChecklistViewController: BaseViewController {
         self.present(bookmarkListVC, animated: true)
     }
     
+    // MARK: - 체크 항목 추가 버튼
+    @objc private func plusButtonTapped() {
+        print(#function)
+        let editModalVC = EditModalViewController()
+        editModalVC.modalTransitionStyle = .crossDissolve
+        editModalVC.modalPresentationStyle = .overCurrentContext
+        editModalVC.selectedChecklistID = selectedChecklistID
+        editModalVC.editAction = .createCheckItem
+        self.present(editModalVC, animated: true)
+    }
+    
     // MARK: - 체크리스트 삭제 버튼 (노티)
     @objc func deleteChecklistNotificationObserver(notification: NSNotification) {
         print(#function)
@@ -171,6 +193,7 @@ class ChecklistViewController: BaseViewController {
         }
     }
     
+    // MARK: - 체크아이템 삭제 apply (노티)
     @objc func deleteCheckItemNotificationObserver(notification: NSNotification) {
         print(#function)
         
@@ -183,34 +206,91 @@ class ChecklistViewController: BaseViewController {
         }
     }
     
+    // MARK: - 체크아이템 내용 수정 apply (노티)
+    @objc func updateCheckItemContentNotificationObserver(notification: NSNotification) {
+        print(#function)
+        
+        // FIXME: 다국어 설정
+        showToast(message: "성공적으로 수정되었습니다!")
+        
+        DispatchQueue.main.async {
+            self.configureChecklistDataSource()
+        }
+    }
+    
+    // MARK: - 체크아이템 메모 설정 apply (노티)
+    @objc func updateCheckItemMemoNotificationObserver(notification: NSNotification) {
+        print(#function)
+        
+        // FIXME: 다국어 설정
+        showToast(message: "성공적으로 수정되었습니다!")
+        
+        DispatchQueue.main.async {
+            self.configureChecklistDataSource()
+        }
+    }
+    
     // MARK: - CollectionView DataSource
     private func configureChecklistDataSource() {
+        print(#function)
+        guard let checkItemTasks = checkItemTasks else {
+            print("checkItemTasks is nil")
+            return
+        }
+        
         let cellRegistration = UICollectionView.CellRegistration<ChecklistCollectionViewCell, CheckItemTable> {
             cell, indexPath, itemIdentifier in
+            
+            let checkItemID = itemIdentifier.id
             
             cell.checkBoxLabel.text = itemIdentifier.content
             cell.checkBoxMemoLabel.text = itemIdentifier.memo
             
-            // Menu
+            // MARK: - 체크아이템 메뉴 버튼
             cell.checkBoxMenuButton.menu = UIMenu(preferredElementSize: .medium, children: [
+                /// 내용 수정
                 UIAction(title: "checklist_checkBoxMenuButton_firstMenu".localized, image: UIImage(systemName: Constant.SFSymbol.editMenuItemIcon), handler: { _ in
                     print("Edit Menu Tapped")
+                    
+                    let editModalVC = EditModalViewController()
+                    
+                    editModalVC.modalTransitionStyle = .crossDissolve
+                    editModalVC.modalPresentationStyle = .overCurrentContext
+                    editModalVC.selectedCheckItemID = checkItemID
+                    editModalVC.editAction = .updateCheckItemContent
+                    
+                    let placeholder = self.checkItemRepository.getCheckItemContent(forId: checkItemID)
+                    editModalVC.textFieldPlaceholder = placeholder
+                    
+                    self.present(editModalVC, animated: true)
                 }),
-                
+                /// 메모 설정
                 UIAction(title: "checklist_checkBoxMenuButton_secondMenu".localized, image: UIImage(systemName: Constant.SFSymbol.addMemoMenuItemIcon), handler: { _ in
                     print("Add Memo Menu Tapped")
+                    
+                    let editModalVC = EditModalViewController()
+                    
+                    editModalVC.modalTransitionStyle = .crossDissolve
+                    editModalVC.modalPresentationStyle = .overCurrentContext
+                    editModalVC.selectedCheckItemID = checkItemID
+                    editModalVC.editAction = .updateCheckItemMemo
+                    
+                    let placeholder = self.checkItemRepository.getCheckItemMemo(forId: checkItemID) ?? "placeholder"
+                    editModalVC.textFieldPlaceholder = placeholder
+                    
+                    self.present(editModalVC, animated: true)
                 }),
-                
+                /// 삭제
                 UIAction(title: "checklist_checkBoxMenuButton_thirdMenu".localized, image: UIImage(systemName: Constant.SFSymbol.deleteMemuItemIcon), attributes: .destructive, handler: { _ in
                     print("Delete Menu Tapped")
+                
+                    let popupVC = DeletePopUpViewController()
                     
-                    let checkItemID = itemIdentifier.id
-                    
-                    let popupVC = PopUpViewController()
                     popupVC.modalTransitionStyle = .crossDissolve
                     popupVC.modalPresentationStyle = .overCurrentContext
                     popupVC.selectedCheckItemID = checkItemID
                     popupVC.popUpAction = .deleteCheckItem
+                    
                     self.present(popupVC, animated: true)
                 })
             ])
@@ -232,19 +312,43 @@ class ChecklistViewController: BaseViewController {
 
 // MARK: - CollectionView Delegate
 extension ChecklistViewController: UICollectionViewDelegate {
-    
+
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(#function)
+
+        guard let selectedItem = checklistDataSource.itemIdentifier(for: indexPath) else {
+            return
+        }
+
+        let isCheckedOfSelectedCell = checkItemRepository.getCheckItemIsChecked(forId: selectedItem.id)
         
-        // FIXME: 체크박스 클릭시 Realm Data 상호작용 및 UI 업데이트 필요
+        guard let isChecked = isCheckedOfSelectedCell else {
+            print("isChecked error")
+            return
+        }
+
         if let cell = collectionView.cellForItem(at: indexPath) as? ChecklistCollectionViewCell {
-            cell.checkBoxButton.layer.backgroundColor = UIColor.tGray1000.cgColor
-            cell.checkBoxButton.setImage(UIImage(systemName: Constant.SFSymbol.checkIcon), for: .normal)
-            cell.tintColor = .tGray100
+            if !isChecked {
+                // False -> True
+                DispatchQueue.main.async {
+                    cell.checkBoxButton.layer.backgroundColor = UIColor.tGray1000.cgColor
+                    cell.checkBoxButton.setImage(UIImage(systemName: Constant.SFSymbol.checkIcon), for: .normal)
+                    cell.tintColor = .tGray100
+                }
+                checkItemRepository.updateCheckItemIsChecked(forId: selectedItem.id, newIsChecked: true)
+            } else {
+                // True -> False
+                DispatchQueue.main.async {
+                    cell.checkBoxButton.layer.backgroundColor = UIColor.tGray100.cgColor
+                    cell.checkBoxButton.setImage(nil, for: .normal)
+                }
+                checkItemRepository.updateCheckItemIsChecked(forId: selectedItem.id, newIsChecked: false)
+            }
         }
     }
-    
+
 }
+
 
 // MARK: - Sheet Delegate
 extension ChecklistViewController: UISheetPresentationControllerDelegate {
