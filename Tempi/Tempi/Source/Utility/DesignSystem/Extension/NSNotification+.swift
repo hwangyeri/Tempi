cb5186c2609c6baaf305efb49699608c220a17bf
hwangyeri <dpfl420@naver.com>
2023-10-19 14:33:18 +0900

    [Feat] NSNotification+ 추가
    - updateCheckItemContent
    - updateCheckItemMemo
    - createChecklist

--- Tempi/Tempi/Source/Utility/DesignSystem/Extension/NSNotification+.swift
+++ Tempi/Tempi/Source/Utility/DesignSystem/Extension/NSNotification+.swift
@@ -11,4 +11,7 @@ extension NSNotification.Name {
     static let deleteChecklist = NSNotification.Name("DeleteChecklist")
     static let deleteCheckItem = NSNotification.Name("DeleteCheckItem")
     static let updateChecklistName = NSNotification.Name("UpdateChecklistName")
+    static let updateCheckItemContent = NSNotification.Name("UpdateCheckItemContent")
+    static let updateCheckItemMemo = NSNotification.Name("UpdateCheckItemMemo")
+    static let createChecklist = NSNotification.Name("CreateChecklist")
 }
