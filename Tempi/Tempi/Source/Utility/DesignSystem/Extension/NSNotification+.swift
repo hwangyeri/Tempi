d0429bfea65fb4e9c39e3dcf0e9d174b0c9a328c
hwangyeri <dpfl420@naver.com>
2023-10-22 17:35:24 +0900

    [Refactor] createChecklist 변경 및 주석 추가
    - createChecklistFromHome 생성
    - createChecklistFromMy 생성

--- Tempi/Tempi/Source/Utility/DesignSystem/Extension/NSNotification+.swift
+++ Tempi/Tempi/Source/Utility/DesignSystem/Extension/NSNotification+.swift
@@ -8,11 +8,28 @@
 import Foundation
 
 extension NSNotification.Name {
+    /// 체크리스트 삭제
     static let deleteChecklist = NSNotification.Name("DeleteChecklist")
+    
+    /// 체크 아이템 삭제
     static let deleteCheckItem = NSNotification.Name("DeleteCheckItem")
+    
+    /// 체크리스트 이름 업데이트
     static let updateChecklistName = NSNotification.Name("UpdateChecklistName")
-    static let updateCheckItemContent = NSNotification.Name("UpdateCheckItemContent")
+    
+    /// 체크 아이템 내용 업데이트
+    static let updateCheckItemContent = NSNotification.Name("UpdateCheckItemContent") 
+    
+    /// 체크 아이템 메모 업데이트
     static let updateCheckItemMemo = NSNotification.Name("UpdateCheckItemMemo")
-    static let createChecklist = NSNotification.Name("CreateChecklist")
+    
+    /// 홈화면에서 체크리스트 생성
+    static let createChecklistFromHome = NSNotification.Name("CreateChecklistFromHome")
+    
+    /// 나의 체크리스트 화면에서 체크리스트 생성
+    static let createChecklistFromMy = NSNotification.Name("CreateChecklistFromMy")
+    
+    /// 체크 아이템 생성
     static let createCheckItem = NSNotification.Name("CreateCheckItem")
 }
+
