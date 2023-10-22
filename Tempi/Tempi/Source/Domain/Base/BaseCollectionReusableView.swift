35b2590e2e404be7d06a951c56c1d538f7ed2c2a
hwangyeri <dpfl420@naver.com>
2023-10-22 17:48:15 +0900

    [Feat] BaseCollectionReusableView 생성

---
+++ Tempi/Tempi/Source/Domain/Base/BaseCollectionReusableView.swift
@@ -0,0 +1,26 @@
+//
+//  BaseCollectionReusableView.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/10/22.
+//
+
+import UIKit
+
+class BaseCollectionReusableView: UICollectionReusableView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+        
+}
