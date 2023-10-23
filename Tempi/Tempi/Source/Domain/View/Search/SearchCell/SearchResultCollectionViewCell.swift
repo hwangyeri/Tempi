9c3d70cac21b0f10c82b06582feafad7e79dfb48
hwangyeri <dpfl420@naver.com>
2023-10-23 16:20:08 +0900

    [Style] 검색 결과 CollectionViewCell UI 구현

---
+++ Tempi/Tempi/Source/Domain/View/Search/SearchCell/SearchResultCollectionViewCell.swift
@@ -0,0 +1,82 @@
+//
+//  SearchResultCollectionViewCell.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/10/22.
+//
+
+import UIKit
+import SnapKit
+
+class SearchResultCollectionViewCell: BaseCollectionViewCell {
+    
+    let backgroundButton = {
+        let view = UIButton()
+        view.layer.cornerRadius = 35
+        view.layer.borderWidth = 0.5
+        view.layer.borderColor = UIColor.tGray400.cgColor
+        view.backgroundColor = UIColor.tGray100
+        view.isUserInteractionEnabled = false
+        return view
+    }()
+    
+    let categoryLabel = {
+       let view = TLabel(
+        text: "searchResult_categoryNameLabel".localized,
+        custFont: .pretendardMediumXS,
+        textColor: .tGray700
+       )
+        view.numberOfLines = 1
+        return view
+    }()
+    
+    let subCategoryNameLabel = {
+        let view = TLabel(
+            text: "searchResult_subCategoryNameLabel".localized,
+            custFont: .pretendardMediumM,
+            textColor: .tGray1000
+        )
+        view.numberOfLines = 1
+        return view
+    }()
+    
+    let chevronImageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: Constant.SFSymbol.chevronRightIcon)
+        view.contentMode = .scaleAspectFit
+        view.tintColor = UIColor.tGray1000
+        return view
+    }()
+    
+    override func configureHierarchy() {
+        self.addSubview(backgroundButton)
+        
+        [categoryLabel, subCategoryNameLabel, chevronImageView].forEach {
+            backgroundButton.addSubview($0)
+        }
+    }
+    
+    override func configureLayout() {
+        backgroundButton.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        categoryLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(19)
+            make.leading.equalToSuperview().inset(30)
+            make.trailing.equalTo(chevronImageView.snp.leading)
+        }
+        
+        subCategoryNameLabel.snp.makeConstraints { make in
+            make.top.equalTo(categoryLabel.snp.bottom).offset(3)
+            make.horizontalEdges.equalTo(categoryLabel)
+        }
+        
+        chevronImageView.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.trailing.equalToSuperview().inset(20)
+            make.size.equalTo(20)
+        }
+    }
+    
+}
