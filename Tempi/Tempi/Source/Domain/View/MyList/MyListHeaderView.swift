64669aa56675a4325b388e39101faa91bcd881f7
hwangyeri <dpfl420@naver.com>
2023-10-23 16:05:51 +0900

    [Style] 나의 리스트 화면 - HeaderView UI 구현

---
+++ Tempi/Tempi/Source/Domain/View/MyList/MyListHeaderView.swift
@@ -0,0 +1,63 @@
+//
+//  MyListHeaderView.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/10/21.
+//
+
+import UIKit
+import SnapKit
+
+class MyListHeaderView: BaseCollectionReusableView {
+    
+    let imageView = {
+        let view = UIImageView()
+        view.image = UIImage(systemName: Constant.SFSymbol.checklistFixedIcon)
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    let titleLabel = {
+        let view = TLabel(
+            text: "",
+            custFont: .pretendardBoldS,
+            textColor: .tGray1000
+        )
+        return view
+    }()
+    
+    override func configureHierarchy() {
+        [imageView, titleLabel].forEach {
+            addSubview($0)
+        }
+    }
+    
+    override func configureLayout() {
+        imageView.snp.makeConstraints { make in
+            make.bottom.equalToSuperview().inset(1)
+            make.leading.equalToSuperview().inset(15)
+            make.size.equalTo(13)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.bottom.equalToSuperview()
+            make.leading.equalTo(imageView.snp.trailing).offset(6)
+        }
+    }
+    
+    func updateLayoutForHiddenImage(isHidden: Bool) {
+        imageView.isHidden = isHidden
+        if isHidden {
+            titleLabel.snp.remakeConstraints { make in
+                make.bottom.equalToSuperview()
+                make.leading.equalToSuperview().inset(20)
+            }
+        } else {
+            titleLabel.snp.remakeConstraints { make in
+                make.bottom.equalToSuperview()
+                make.leading.equalTo(imageView.snp.trailing).offset(6)
+            }
+        }
+    }
+    
+}
