d6a42312884b753c4d2f63d53cbdd096eab3c2f7
hwangyeri <dpfl420@naver.com>
2023-10-30 14:45:52 +0900

    [Comment] LineSpacing+ 주석 설명 추가

--- Tempi/Tempi/Source/Utility/DesignSystem/Extension/LineSpacing+.swift
+++ Tempi/Tempi/Source/Utility/DesignSystem/Extension/LineSpacing+.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 extension UILabel {
+    
+    // MARK: - 텍스트 행간 설정
     func setAttributedTextWithLineSpacing(_ text: String, lineSpacing: CGFloat) {
         let attrString = NSMutableAttributedString(string: text.localized)
         let paragraphStyle = NSMutableParagraphStyle()
