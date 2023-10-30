b15e99be6e957317a2d83e53105811c07eead350
hwangyeri <dpfl420@naver.com>
2023-10-30 15:18:09 +0900

    [Style] tColor DarkMode 대응 컬러을 위한 전체 컬러 수정 작업

--- Tempi/Tempi/Source/Utility/DesignSystem/Extension/Color+.swift
+++ Tempi/Tempi/Source/Utility/DesignSystem/Extension/Color+.swift
@@ -10,46 +10,43 @@ import UIKit
 extension UIColor {
 
     enum AssetsColor {
-        case mainBack
-        case point
-        case tGray100
-        case tGray200
-        case tGray300
-        case tGray400
-        case tGray500
-        case tGray600
-        case tGray700
-        case tGray800
-        case tGray900
-        case tGray1000
+        case deleteCancel // 삭제 모달 화면 - 삭제 알럿 취소 버튼 배경색
+        case divider // 구분선 배경색
+        case homeBackground // 홈화면 - 상단 백뷰 배경색
+        case homeImage // 홈화면 - 컬렉션셀 이미지 배경색
+        case listBackground // 체크리스트 추가 화면, 나의 리스트 화면 - 컬렉션뷰 배경색
+        case point // 포인트 컬러
+        case searchBackground // 홈 화면 - 서치 버튼 배경색
+        case searchBorder // 홈 화면 - 서치 버튼 보더 컬러
+        case searchPlaceholder // 홈 화면 - 서치 버튼 라벨 컬러
+        case tButtonDisable // tButton - 비활성화 배경색
+        case textFieldBackground // 편집 모달 화면 - 텍스트필드 배경색, 카테고리 상세 화면 - 컬렉션셀 비활성화 배경색
     }
 
     static func tColor(_ name: AssetsColor) -> UIColor? {
         switch name {
-        case .mainBack:
-            return UIColor(named: "mainBack") // 000000 - 434343
+        case .deleteCancel:
+            return UIColor(named: "deleteCancel") // 999999 - 666666
+        case .divider:
+            return UIColor(named: "divider") // F3F3F3 - 434343
+        case .homeBackground:
+            return UIColor(named: "homeBackground") // 000000 - 161616
+        case .homeImage:
+            return UIColor(named: "homeImage") // 434343 - F3F3F3
+        case .listBackground:
+            return UIColor(named: "listBackground") // EEEFEF - 1C1C1E
         case .point:
             return UIColor(named: "point") // FF7B88 - FF7B88
-        case .tGray100:
-            return UIColor(named: "tGray100") // FFFFFF - 000000
-        case .tGray200:
-            return UIColor(named: "tGray200") // F3F3F3 - 434343
-        case .tGray300:
-            return UIColor(named: "tGray300") // EFEFEF - 666666
-        case .tGray400:
-            return UIColor(named: "tGray400") // D9D9D9 - 999999
-        case .tGray500:
-            return UIColor(named: "tGray500") // CCCCCC - B7B7B7
-        case .tGray600:
-            return UIColor(named: "tGray600") // B7B7B7 - CCCCCC
-        case .tGray700:
-            return UIColor(named: "tGray700") // 999999 - D9D9D9
-        case .tGray800:
-            return UIColor(named: "tGray800") // 666666 - EFEFEF
-        case .tGray900:
-            return UIColor(named: "tGray900") // 434343 - F3F3F3
-        case .tGray1000:
-            return UIColor(named: "tGray1000") // 000000 - FFFFFF
+        case .searchBackground:
+            return UIColor(named: "searchBackground") // 434343 - 434343
+        case .searchBorder:
+            return UIColor(named: "searchBorder") // 666666 - 666666
+        case .searchPlaceholder:
+            return UIColor(named: "searchPlaceholder") // 999999 - 999999
+        case .tButtonDisable:
+            return UIColor(named: "tButtonDisable") // D9D9D9 - B7B7B7
+        case .textFieldBackground:
+            return UIColor(named: "textFieldBackground") // F3F3F3 - 434343
         }
     }
 
