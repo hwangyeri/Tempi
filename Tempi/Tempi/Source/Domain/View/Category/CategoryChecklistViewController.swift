13eee7d0d0d74d6cef730f90cd5973509895eb35
hwangyeri <dpfl420@naver.com>
2023-11-02 21:13:04 +0900

    [Fix] 전체 선택/해제 버튼 클릭시 전체 셀 선택 안되는 오류 해결
    - selectAllCheckBox, tButton 버튼 선택여부 및 UI update 로직 변경
    - didSelectItemAt 컬렉션뷰 셀 선택여부 및 UI update 로직 변경

--- Tempi/Tempi/Source/Domain/View/Category/CategoryChecklistViewController.swift
+++ Tempi/Tempi/Source/Domain/View/Category/CategoryChecklistViewController.swift
@@ -22,16 +22,20 @@ class CategoryChecklistViewController: BaseViewController {
             .filter { $0.subCategoryName == subCategoryName }
             .map { $0.checkItem }
         
-        let uniqueCheckItems = Array(Set(checkItems)) // 중복 제거
+        // 중복 제거
+        let uniqueCheckItems = Array(Set(checkItems))
         return uniqueCheckItems
     }
     
-    private var selectedIndexPaths: [IndexPath] = []
     private var selectedItems: [String] = [] {
         didSet {
             DispatchQueue.main.async {
+                // 선택된 아이템 수
                 self.mainView.selectedItemCountLabel.text = "category_checklist_itemCountLabel".localized(with: self.selectedItems.count)
             }
+            // tButton 활성화/비활성화
+            self.mainView.tButtonIsSelected = !self.selectedItems.isEmpty
+            print(self.selectedItems, self.selectedItems.count)
         }
     }
     
@@ -67,72 +71,34 @@ class CategoryChecklistViewController: BaseViewController {
         DispatchQueue.main.async {
             self.mainView.checklistNameLabel.text = "category_checklist_checklistNameLabel".localized(with: subCategoryName)
             self.mainView.totalCountLabel.text = "category_checklist_totalCountLabel".localized(with: self.checkItemList.count)
-            self.mainView.selectedItemCountLabel.text = "category_checklist_itemCountLabel".localized(with: self.selectedItems.count)
         }
     }
     
     // MARK: - 전체 선택/해제 버튼
     @objc private func selectAllCheckBoxTapped() {
-        if mainView.selectAllCheckBox.isSelected {
-            // 전체 해제
-            selectedIndexPaths.removeAll()
-            selectedItems.removeAll()
-            print(selectedItems, "-- 선택된 체크 아이템 --")
-            mainView.selectAllCheckBox.isSelected = false
-        } else {
-            // 전체 선택
-            selectedIndexPaths = (0..<checkItemList.count).map { IndexPath(item: $0, section: 0) }
+        if !mainView.selectAllCheckBox.isSelected {
             selectedItems = checkItemList
-            print(selectedItems, "-- 선택된 체크 아이템 --")
-            mainView.selectAllCheckBox.isSelected = true
-        }
-       
-        // 선택된 항목만 선택된 상태로 설정
-        for indexPath in selectedIndexPaths {
-            if let cell = mainView.categoryChecklistCollectionView.cellForItem(at: indexPath) as? CategoryChecklistCollectionViewCell {
-                DispatchQueue.main.async {
-                    cell.checkBoxButton.layer.backgroundColor = UIColor.label.cgColor
-                    cell.checkBoxButton.setImage(UIImage(systemName: Constant.SFSymbol.checkIcon), for: .normal)
-                    cell.tintColor = .systemBackground
-                }
-            }
+            self.mainView.tButtonIsSelected = true
+//            self.mainView.selectAllCheckBox.isSelected = true
+            self.mainView.selectedAllCheckBoxIsSelected = true
+            print(mainView.selectAllCheckBox.isSelected)
+        } else {
+            selectedItems.removeAll()
+            self.mainView.tButtonIsSelected = false
+//            self.mainView.selectAllCheckBox.isSelected = false
+            self.mainView.selectedAllCheckBoxIsSelected = false
+            print(mainView.selectAllCheckBox.isSelected)
         }
         
-        // 나머지 항목은 선택 해제 상태로 설정
-        for itemIndex in 0..<checkItemList.count {
-            if !selectedIndexPaths.contains(IndexPath(item: itemIndex, section: 0)) {
-                if let cell = mainView.categoryChecklistCollectionView.cellForItem(at: IndexPath(item: itemIndex, section: 0)) as? CategoryChecklistCollectionViewCell {
-                    DispatchQueue.main.async {
-                        cell.checkBoxButton.layer.backgroundColor = UIColor.systemBackground.cgColor
-                        cell.checkBoxButton.setImage(nil, for: .normal)
-                    }
-                }
-            }
-        }
-        
-        // 전체 선택/해제 Button, Label UI 업데이트
-        DispatchQueue.main.async {
-            if self.mainView.selectAllCheckBox.isSelected {
-                self.mainView.selectAllLabel.text = "category_checklist_selectAllLabel_unSelectAll".localized
-                self.mainView.selectAllCheckBox.setImage(UIImage(systemName: Constant.SFSymbol.checkIcon), for: .normal)
-                self.mainView.selectAllCheckBox.backgroundColor = .label
-                self.mainView.selectAllCheckBox.tintColor = .systemBackground
-                self.mainView.tButton.isEnabled = true
-                self.mainView.tButton.backgroundColor = .label
-            } else {
-                self.mainView.selectAllLabel.text = "category_checklist_selectAllLabel_selectAll".localized
-                self.mainView.selectAllCheckBox.setImage(nil, for: .normal)
-                self.mainView.selectAllCheckBox.backgroundColor = .clear
-                self.mainView.tButton.isEnabled = false
-                self.mainView.tButton.backgroundColor = .tButtonDisable
-            }
-        }
+        var snapshot = NSDiffableDataSourceSnapshot<Int, String>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(checkItemList)
+        categoryChecklistDataSource.apply(snapshot)
     }
     
     // MARK: - 내 리스트에 추가하기 버튼
     @objc private func addToMyListButtonTapped() {
         print(#function)
-        
         let addChecklistVC = AddChecklistViewController()
         
         addChecklistVC.subCategoryName = subCategoryName
@@ -150,7 +116,7 @@ class CategoryChecklistViewController: BaseViewController {
         let cellRegistration = UICollectionView.CellRegistration<CategoryChecklistCollectionViewCell, String> {
             cell, indexPath, itemIdentifier in
             cell.checkBoxLabel.text = itemIdentifier
-//            cell.backgroundColor = .red
+            cell.cellIsSelected = self.selectedItems.contains(itemIdentifier)
         }
         
         categoryChecklistDataSource = UICollectionViewDiffableDataSource(collectionView: mainView.categoryChecklistCollectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
@@ -171,38 +137,26 @@ extension CategoryChecklistViewController: UICollectionViewDelegate {
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print(#function)
-        
-        if let cell = collectionView.cellForItem(at: indexPath) as? CategoryChecklistCollectionViewCell {
-            
-            guard let selectedItem = categoryChecklistDataSource.itemIdentifier(for: indexPath) else {
-                // FIXME: Toast alert, Alert 및 예외 처리
-                return
-            }
-            
-            if let selectedIndex = selectedIndexPaths.firstIndex(of: indexPath) {
-                // 체크박스 선택 해제시, 배열에서 삭제 및 UI 업데이트
-                selectedIndexPaths.remove(at: selectedIndex)
-                cell.checkBoxButton.layer.backgroundColor = UIColor.systemBackground.cgColor
-                cell.checkBoxButton.setImage(nil, for: .normal)
-                
-                if let itemIndex = selectedItems.firstIndex(of: selectedItem) {
-                    selectedItems.remove(at: itemIndex)
-                }
-            } else {
-                // 체크박스 선택 시, 배열에 추가 및 UI 업데이트
-                selectedIndexPaths.append(indexPath)
-                cell.checkBoxButton.layer.backgroundColor = UIColor.label.cgColor
-                cell.checkBoxButton.setImage(UIImage(systemName: Constant.SFSymbol.checkIcon), for: .normal)
-                cell.tintColor = .systemBackground
-                
-                selectedItems.append(selectedItem)
-            }
+        guard let item = categoryChecklistDataSource.itemIdentifier(for: indexPath) else {
+            // FIXME: Toast alert, Alert
+            print("item Error")
+            return
         }
         
-        // tButton 활성화/비활성화
-        mainView.tButton.isEnabled = !selectedItems.isEmpty
-        mainView.tButton.backgroundColor = selectedItems.isEmpty ? .tButtonDisable : .label
-        //print(selectedItems, "-- 선택된 체크 아이템 --")
+        guard let cell = collectionView.cellForItem(at: indexPath) as? CategoryChecklistCollectionViewCell else {
+            print("cell Error")
+            return
+        }
+        
+        cell.cellIsSelected.toggle()
+        
+        if cell.cellIsSelected {
+            self.selectedItems.append(item)
+        } else {
+            if let index = self.selectedItems.firstIndex(of: item) {
+                self.selectedItems.remove(at: index)
+            }
+        }
     }
 
 }
