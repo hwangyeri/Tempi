5e542d1cc07125c1dda248beaa6a2084ac792aed
hwangyeri <dpfl420@naver.com>
2023-12-09 19:04:54 +0900

    [Feat] BaseTableViewCell 생성

---
+++ Tempi/Tempi/Source/Domain/Base/BaseTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  BaseTableViewCell.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2023/12/09.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
+        self.backgroundColor = UIColor.systemBackground
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy() { }
+    
+    func configureLayout() { }
+
+}
