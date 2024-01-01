a8f5901bca93be30f4bd6d580b5fce790bfd470e
hwangyeri <dpfl420@naver.com>
2024-01-01 15:41:20 +0900

    [Refactor] SettingView CollectionView -> TableView 변경

--- Tempi/Tempi/Source/Domain/View/Setting/SettingView.swift
+++ Tempi/Tempi/Source/Domain/View/Setting/SettingView.swift
@@ -10,48 +10,25 @@ import SnapKit
 
 final class SettingView: BaseView {
     
-    let titleLabel = {
-       let view = UILabel()
+    let tableView = {
+        let view = UITableView(frame: .zero, style: .grouped)
+        view.estimatedRowHeight = 100
+        view.rowHeight = UITableView.automaticDimension
+        view.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.identifier)
+        view.backgroundColor = .systemBackground
         return view
     }()
     
-    lazy var setupCollectionView = UICollectionView(frame: .zero, collectionViewLayout: configureSetupCollectionLayout())
-    
     override func configureHierarchy() {
-        [titleLabel, setupCollectionView].forEach {
-            addSubview($0)
-        }
+        self.addSubview(tableView)
     }
     
     override func configureLayout() {
-        titleLabel.snp.makeConstraints { make in
-            
-        }
-        
-        setupCollectionView.snp.makeConstraints { make in
-            
+        tableView.snp.makeConstraints { make in
+            make.verticalEdges.equalTo(self.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
         }
     }
     
-    private func configureSetupCollectionLayout() -> UICollectionViewLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(75))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, repeatingSubitem: item, count: 1)
-        group.interItemSpacing = .fixed(10)
-        
-        let section = NSCollectionLayoutSection(group: group)
-        section.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 20, bottom: 10, trailing: 20)
-        section.interGroupSpacing = 10
-        
-        let configuration = UICollectionViewCompositionalLayoutConfiguration()
-        configuration.scrollDirection = .vertical
-        
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        layout.configuration = configuration
-        
-        return layout
-    }
-    
+   
 }
