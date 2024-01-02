141bea173b887f6e98ccdb4b14561c4b65ec52b3
hwangyeri <dpfl420@naver.com>
2024-01-02 14:58:46 +0900

    [Feat] Alert+ showDeleteAlert 메서드 추가, 다국어 설정

--- Tempi/Tempi/Source/Utility/DesignSystem/Extension/Alert+.swift
+++ Tempi/Tempi/Source/Utility/DesignSystem/Extension/Alert+.swift
@@ -49,11 +49,26 @@ extension UIViewController {
     
     func showAlert(title: String, message: String, handler: (() -> ())? = nil ) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let ok = UIAlertAction(title: "확인", style: .default) { _ in
+        let ok = UIAlertAction(title: "showAlert_ok".localized, style: .default) { _ in
             handler?()
         }
         
         alert.addAction(ok)
         present(alert, animated: true)
     }
+    
+    func showDeleteAlert(title: String, message: String, handler: (() -> ())? = nil ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let delete = UIAlertAction(title: "showAlert_delete".localized, style: .destructive) { _ in
+            handler?()
+        }
+        let cancel = UIAlertAction(title: "showAlert_cancel".localized, style: .cancel)
+        
+        alert.addAction(delete)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true)
+    }
+    
+    
 }
