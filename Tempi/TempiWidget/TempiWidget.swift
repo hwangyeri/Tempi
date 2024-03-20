55c3099155f5fc5dd9ee3e4ee3f161aeeeb98183
hwangyeri <dpfl420@naver.com>
2024-03-20 17:16:37 +0900

    [Feat] Widget Extension 추가

---
+++ Tempi/TempiWidget/TempiWidget.swift
@@ -0,0 +1,80 @@
+//
+//  TempiWidget.swift
+//  TempiWidget
+//
+//  Created by Yeri Hwang on 2024/03/20.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), emoji: "😀")
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), emoji: "😀")
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+        var entries: [SimpleEntry] = []
+
+        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
+        let currentDate = Date()
+        for hourOffset in 0 ..< 5 {
+            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+            let entry = SimpleEntry(date: entryDate, emoji: "😀")
+            entries.append(entry)
+        }
+
+        let timeline = Timeline(entries: entries, policy: .atEnd)
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let emoji: String
+}
+
+struct TempiWidgetEntryView : View {
+    var entry: Provider.Entry
+
+    var body: some View {
+        VStack {
+            Text("Time:")
+            Text(entry.date, style: .time)
+
+            Text("Emoji:")
+            Text(entry.emoji)
+        }
+    }
+}
+
+struct TempiWidget: Widget {
+    let kind: String = "TempiWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                TempiWidgetEntryView(entry: entry)
+                    .containerBackground(.fill.tertiary, for: .widget)
+            } else {
+                TempiWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("My Widget")
+        .description("This is an example widget.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    TempiWidget()
+} timeline: {
+    SimpleEntry(date: .now, emoji: "😀")
+    SimpleEntry(date: .now, emoji: "🤩")
+}
---
+++ Tempi/TempiWidget/TempiWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TempiWidgetBundle.swift
+//  TempiWidget
+//
+//  Created by Yeri Hwang on 2024/03/20.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TempiWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TempiWidget()
+    }
+}
