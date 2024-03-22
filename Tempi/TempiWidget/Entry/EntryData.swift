f5ccfc484e5d69d460e78291bcd00d9b31007a19
hwangyeri <dpfl420@naver.com>
2024-03-22 18:23:02 +0900

    [Feat] EntryData 생성

---
+++ Tempi/TempiWidget/Entry/EntryData.swift
@@ -0,0 +1,20 @@
+//
+//  EntryData.swift
+//  Tempi
+//
+//  Created by Yeri Hwang on 2024/03/20.
+//
+
+import WidgetKit
+
+// 위젯을 구성하기 위해 필요한 Data
+struct ChecklistEntry: TimelineEntry {
+    var date: Date
+    var checklistName: [String]
+    var createdAt: [Date]
+}
+
+//struct ChecklistEntry: TimelineEntry {
+//    var checklistName: String
+//    var date: Date
+//}
